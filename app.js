1239a60196b3a6ef42abdca9ba8d5184868c4509
all-an <all-an@hotmail.com>
2022-04-30 12:44:00 -0300

    botão filtro

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ let nomePag = '';
 
 function trocaNome(nomePagina){
     localStorage.setItem('nomePagina', JSON.stringify(nomePagina)) 
+    document.getElementById("menuPrincipal").classList.toggle("esconder");
 }
 
 document.getElementById("principal").innerHTML = JSON.parse(localStorage.getItem('nomePagina'));
+
+document.getElementById("menuBotao").onclick = function() {mostrar()};
+
+function mostrar() {
+    document.getElementById("menuPrincipal").classList.toggle("mostrar");
+}
